ca3fbed9276f0e2af74a430db681af7f193249ca
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-03-31 15:25:46 -0500

    Add final styling and final funcitonality for project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DOM Elements//
 const cityHeader = document.querySelector('.city-header');
 const currentTemp = document.querySelector('.current-temp');
 const conditonText = document.querySelector('.condition-text');
@@ -5,17 +6,32 @@ const currentConditionIcon = document.querySelector('.current-day-icon');
 const foreCastDaysArr = document.querySelectorAll('.days');
 const dateContainerArr = document.querySelectorAll('.date-container');
 const minMaxArr = document.querySelectorAll('.max-min');
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.getElementById('search');
+const unitBtn = document.getElementById('unit');
+const foreCastDaysContainers = document.querySelectorAll('.day-icon');
+const form = document.querySelector('form');
+const body = document.querySelector('body');
+//Current weather object returned by function//
+let currentWeatherInfo;
+//Get weather async funciton//
+async function getWeather(location) {
+  try {
+    const forecastResponse = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=c8924334cfb94f5498e165605242003&q=${location}&days=3`
+    );
+    const forecastData = await forecastResponse.json();
+    console.log(forecastData);
 
-(async function getWeather(location) {
-  const forecastResponse = await fetch(
-    'http://api.weatherapi.com/v1/forecast.json?key=c8924334cfb94f5498e165605242003&q=chicago&days=3'
-  );
-  const forecastData = await forecastResponse.json();
-  console.log(forecastData);
-
-  console.log(getJsonData(forecastData));
-})();
-
+    console.log(getJsonData(forecastData));
+  } catch (err) {
+    console.log(err);
+    alert('Error please try again');
+  }
+}
+// //Function invocation//
+getWeather('Berlin');
+//Grabbing the JSON data//
 function getJsonData(forecast) {
   const weatherInfo = {
     city: forecast.location.name,
@@ -25,6 +41,7 @@ function getJsonData(forecast) {
     forecastDays: [
       {
         date: forecast.forecast.forecastday[0].date,
+        conditionText: forecast.forecast.forecastday[0].day.condition.text,
         maxTempFaren: forecast.forecast.forecastday[0].day.maxtemp_f,
         minTempFaren: forecast.forecast.forecastday[0].day.mintemp_f,
         maxTempCels: forecast.forecast.forecastday[0].day.maxtemp_c,
@@ -32,6 +49,7 @@ function getJsonData(forecast) {
       },
       {
         date: forecast.forecast.forecastday[1].date,
+        conditionText: forecast.forecast.forecastday[1].day.condition.text,
         maxTempFaren: forecast.forecast.forecastday[1].day.maxtemp_f,
         minTempFaren: forecast.forecast.forecastday[1].day.mintemp_f,
         maxTempCels: forecast.forecast.forecastday[1].day.maxtemp_c,
@@ -39,6 +57,7 @@ function getJsonData(forecast) {
       },
       {
         date: forecast.forecast.forecastday[2].date,
+        conditionText: forecast.forecast.forecastday[2].day.condition.text,
         maxTempFaren: forecast.forecast.forecastday[2].day.maxtemp_f,
         minTempFaren: forecast.forecast.forecastday[2].day.mintemp_f,
         maxTempCels: forecast.forecast.forecastday[2].day.maxtemp_c,
@@ -48,27 +67,50 @@ function getJsonData(forecast) {
   };
 
   displayData(weatherInfo);
+  displayWeatherIcon(weatherInfo.conditionText, currentConditionIcon);
+  setBackgroundColor(weatherInfo.conditionText);
+  currentWeatherInfo = weatherInfo;
 
   return weatherInfo;
 }
-
+//Function to display data onto DOM//
 function displayData(obj) {
-  cityHeader.textContent = obj.city;
-  currentTemp.textContent = obj.fahrenheitTemp;
+  if (unitBtn.textContent === '°F') {
+    currentTemp.textContent = Math.round(obj.fahrenheitTemp) + '°F';
+  } else if (unitBtn.textContent === '°C') {
+    currentTemp.textContent = Math.round(obj.celciusTemp) + '°C';
+  }
   conditonText.textContent = obj.conditionText;
-  const daysArr = obj.forecastDays;
+  cityHeader.textContent = obj.city;
 
+  const daysArr = obj.forecastDays;
+  //Looping through each of the three days//
   for (let i = 0; i < daysArr.length; i++) {
     let date = daysArr[i].date;
-    let maxTemp = daysArr[i].maxTempFaren;
-    let minTemp = daysArr[i].minTempFaren;
+    let maxTempFarenheit = daysArr[i].maxTempFaren;
+    let minTempFarenheit = daysArr[i].minTempFaren;
+    let maxTempCelsius = daysArr[i].maxTempCels;
+    let minTempCelsius = daysArr[i].minTempCels;
 
     dateContainerArr[i].textContent = getDayName(date);
-
-    minMaxArr[i].textContent = maxTemp + ' ' + minTemp;
+    displayWeatherIcon(daysArr[i].conditionText, foreCastDaysContainers[i]);
+    //Looping each minMax arrays children p elements//
+    for (let j = 0; j < minMaxArr[i].children.length; j++) {
+      if (unitBtn.textContent === '°F') {
+        minMaxArr[i].children[0].textContent =
+          Math.round(maxTempFarenheit) + '°F';
+        minMaxArr[i].children[1].textContent =
+          Math.round(minTempFarenheit) + '°F';
+      } else if (unitBtn.textContent === '°C') {
+        minMaxArr[i].children[0].textContent =
+          Math.round(maxTempCelsius) + '°C';
+        minMaxArr[i].children[1].textContent =
+          Math.round(minTempCelsius) + '°C';
+      }
+    }
   }
 }
-
+//Function to convert dates to day names//
 function getDayName(date) {
   const today = new Date();
   const day = new Date(date);
@@ -94,3 +136,78 @@ function getDayName(date) {
 
   return shortDayName;
 }
+//Event listner for searching cities//
+searchBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  if (searchInput.value.trim() === '') {
+    // If empty, set custom validity and report it
+    searchInput.setCustomValidity('Please enter city or zip code');
+    form.reportValidity();
+  } else {
+    // If not empty, clear custom validity and proceed with weather retrieval
+    searchInput.setCustomValidity('');
+    let searchValue = searchInput.value;
+    await getWeather(searchValue);
+    searchInput.value = '';
+  }
+});
+
+//Function to display icons//
+function displayWeatherIcon(conditonText, imgContainer) {
+  if (conditonText === 'Clear' || conditonText === 'clear') {
+    imgContainer.src = '/icons/clear.png';
+  } else if (conditonText.includes('rain') || conditonText.includes('Rain')) {
+    imgContainer.src = '/icons/rain.png';
+  } else if (
+    conditonText.includes('cloudy') ||
+    conditonText.includes('Cloudy')
+  ) {
+    imgContainer.src = 'icons/cloudy.png';
+  } else if (conditonText.includes('snow') || conditonText.includes('Snow')) {
+    imgContainer.src = 'icons/snow.png';
+  } else if (conditonText.includes('Sunny')) {
+    imgContainer.src = 'icons/sunny.png';
+  } else if (conditonText.includes('Mist')) {
+    imgContainer.src = 'icons/mist.png';
+  } else if (conditonText.includes('Overcast')) {
+    imgContainer.src = 'icons/overcast.png';
+  }
+}
+
+function setBackgroundColor(text) {
+  if (text.includes('Sunny')) {
+    body.style.backgroundImage = 'var(--blue)';
+  } else if (
+    text.includes('Rain') ||
+    text.includes('Cloudy') ||
+    text.includes('Mist') ||
+    text.includes('Overcast') ||
+    text.includes('cloudy')
+  ) {
+    body.style.backgroundImage = 'var(--grey)';
+  } else if (text.includes('Clear')) {
+    body.style.backgroundImage = 'var(--night)';
+  } else if (
+    text.includes('Snow') ||
+    text.includes('snow') ||
+    text.includes('Flurries')
+  ) {
+    body.style.backgroundImage = 'var(--snow)';
+  }
+}
+
+// //Event for changing units//
+unitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (unitBtn.textContent === '°F') {
+    unitBtn.textContent = '°C';
+    displayData(currentWeatherInfo);
+    // displayData(weatherInfo);
+  } else if (unitBtn.textContent === '°C') {
+    unitBtn.textContent = '°F';
+    // displayData(weatherInfo);
+    displayData(currentWeatherInfo);
+  }
+});
